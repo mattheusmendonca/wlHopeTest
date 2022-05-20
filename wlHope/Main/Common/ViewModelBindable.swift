716e385ed969bd8dcab28e9d4e026701753f09e8
mattheusmendonca <teus2002ppbarbosa@outlook.com>
2022-05-20 02:30:31 -0300

    A morte

--- wlHope/Main/Common/ViewModelBindable.swift
+++ wlHope/Main/Common/ViewModelBindable.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import UIKit
+
+protocol ViewModelBindable: AnyObject {
+    associatedtype ViewModelType
+
+    var viewModel: ViewModelType? { get set }
+
+    func bindViewModel()
+}
+
+extension ViewModelBindable where Self: UIView {
+    func bind(to model: Self.ViewModelType) {
+        self.viewModel = model
+        self.bindViewModel()
+    }
+}
+
+extension ViewModelBindable where Self: UIViewController {
+    func bind(to model: Self.ViewModelType) {
+        self.viewModel = model
+        self.loadViewIfNeeded()
+        self.bindViewModel()
+    }
+}
+
+extension ViewModelBindable where Self: UITableViewCell {
+    func bind(to model: Self.ViewModelType) {
+        self.viewModel = model
+        self.bindViewModel()
+    }
+}
+
+extension ViewModelBindable where Self: UICollectionViewCell {
+    func bind(to model: Self.ViewModelType) {
+        self.viewModel = model
+        self.bindViewModel()
+    }
+}
